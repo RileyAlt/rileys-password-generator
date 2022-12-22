18676732bc3cf08b584a1cf2cb1cdf562e833794
RileyAlt <rileyaltenburg@gmail.com>
2022-12-21 19:27:07 -0600

    Added Check for uppercase

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,36 @@
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowerCase = ["a", 'b']
+
 // Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+function generatePassword(canUseUpperCase) {
+  if (canUseUpperCase) {
+    return "ABCD";
+  } 
+  return "abcd";
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var shouldUseUpperCase = confirm("Do you want to use upper case?");
+
+  if (shouldUseUpperCase) {
+    console.log("I am allowed to use uppercase");
+  } else {
+    console.log("I am NOT allowed to use uppercase");
+  }
+
+  var password = generatePassword(shouldUseUpperCase);
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+
+
+  passwordText.value = password;
 }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
