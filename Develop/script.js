7bd4d13f1779f6ec98f033718a844059f607ca17
RileyAlt <rileyaltenburg@gmail.com>
2022-12-21 20:42:00 -0600

    Added speical characters and numbers

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,56 @@
-const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const lowerCase = ["a", 'b']
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+const lowerCaseLetters = "abcdefghijklmkopqrstuvwxyz".split('');
+const specialCharacters = "!#$%&\"'()*+,-./:;<=>?@[\]^_`{|}~".split('');
+const numbericCharacters = "0123456789".split('');
 
 // Assignment code here
-function generatePassword(canUseUpperCase) {
+function generatePassword(passwordLength, canUseUpperCase, canUseLowerCase, canUseSpecialCharacters, canUseNumbericCharacters) {
+
+  var charactersICanUse = []
   if (canUseUpperCase) {
-    return "ABCD";
-  } 
-  return "abcd";
+    charactersICanUse = charactersICanUse.concat(upperCaseLetters);
+  }
+
+  if (canUseLowerCase) {
+    charactersICanUse = charactersICanUse.concat(lowerCaseLetters);
+  }
+
+  if (canUseSpecialCharacters) {
+    charactersICanUse = charactersICanUse.concat(specialCharacters);
+  }
+
+  if (canUseNumbericCharacters) {
+    charactersICanUse = charactersICanUse.concat(numbericCharacters);
+  }
+
+  // Need to enforce passwordLength
+  var mySecretPassword = "";
+  for(var i = 0; i < passwordLength; i++) {
+    var randomCharacter = charactersICanUse[Math.floor(Math.random()*charactersICanUse.length)];
+    mySecretPassword += randomCharacter;
+  }
+
+  return mySecretPassword;
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
 
+  var passwordLength = prompt("Pick a length for your password. (8 to 128 characters)");
   var shouldUseUpperCase = confirm("Do you want to use upper case?");
+  var shouldUseLowerCase = confirm("Do you want to use lower case?");
+  var shouldUseSpecialCharacters = confirm("Do you want to use special characters?");
+  var shouldUseNumbericCharacters = confirm("Do you want to use Numbers?");
+  var password = generatePassword(passwordLength, shouldUseUpperCase, shouldUseLowerCase, shouldUseSpecialCharacters, shouldUseNumbericCharacters);
 
-  if (shouldUseUpperCase) {
-    console.log("I am allowed to use uppercase");
-  } else {
-    console.log("I am NOT allowed to use uppercase");
-  }
-
-  var password = generatePassword(shouldUseUpperCase);
   var passwordText = document.querySelector("#password");
-
-
 
 
   passwordText.value = password;
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
